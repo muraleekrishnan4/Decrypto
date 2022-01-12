951a47c34049877b6b220930a356ddb39bc89aff
Muraleekrishnan <muraleekrishnanunnithanr4@gmail.com>
2022-01-12 18:26:33 +0530

    Added new decryptor
    Made CLI user-friendly
    
    New algorithm
    (+) Hex decryptor

--- decrypto.py
+++ decrypto.py
@@ -1,3 +1,4 @@
+import argparse
 import base64
 
 
@@ -5,74 +6,104 @@ import base64
 class Decrypto:
 
 	base64_decoded = ""
+	hex_decoded = ""
 
 	###Methods###
 
+	def __init__(self, cipher, mode):
+		self.cipher = cipher
+		self.mode = mode
+
+	def attack(self):
+
+		if self.mode.lower() == 'magic':
+			self.base64()
+			self.hex()
+
+			self.printall()
+
+
+		elif self.mode.lower() == 'base64':
+			self.base64()
+
+		elif self.mode.lower() == 'hex':
+			self.hex()
+
+		else:
+			print("\nInvalid Argument")
+
+
+	#Hex
+	def hex(self):
+		print("\n***Hexcode***")
+		print("Decoding...")
+		try:
+			hex_bytes = bytes.fromhex(self.cipher)
+			self.hex_decoded = hex_bytes.decode('ascii')
+			print("Decoded...")
+			print("Decoded Message: ",self.hex_decoded)
+		except:
+			print("Unable to decode")
+
+
+
 	#Base64
-	def base64(self, cipher):
+	def base64(self):
 		print("\n***Base64***")
 		print("Decoding...")
 		try:
-			base64_bytes = cipher.encode('ascii')
+			base64_bytes = self.cipher.encode('ascii')
 			decoded_bytes = base64.b64decode(base64_bytes)
 			self.base64_decoded = decoded_bytes.decode('ascii')
-			print("Decoded")
+			print("Decoded...")
+			print("Decoded Message: ",self.base64_decoded)
 		except:
 			print("Unable to decode")
 
 
 	#Magic
-	def magic(self, cipher):
+	def magic(self):
 		print("\nStarting Magic mode...")
 		print("This is try out all possible methods to decrypt. Please wait...")
 
 		#starting attack
-		self.base64(cipher)
-
-	#Printing the decoded string
-	def print(self, option):
-		print("\n*****************\nDecoded Messages \n*****************")
-
-		if option == 1: #Magic
-			print("Base64 - ",self.base64_decoded)
-		elif option == 2: #Base64
-			print("Base64 - ",self.base64_decoded)
-
-
-		print("\n")
+		self.base64()
 
 
 
 
-###Functions###
+	#Print all decoded
+	def printall(self):
+		print("\nDecoded Strings","\n***************")
 
-#Prints Menu
-def menu():
-	print("""1. Magic mode (Attempts all methods) 
-2. Base64""")
-	return int(input("Choose an option: "))
-
-
-
-#Welcome Message
-print("Welcome to Decrypto")
-
-cipher = input("Enter the string: ")
-
-#Prints Main menu and returns user option
-option = menu()
-
-#Creating object
-dc = Decrypto()
+		if self.base64_decoded != "":
+			print("Base64 - ",self.base64_decoded) #Base64
+		
+		if self.hex_decoded != "":
+			print("Hex - ", self.hex_decoded) #Hexcode
 
 
-if option == 1:
-	dc.magic(cipher)
-elif option == 2:
-	dc.base64(cipher)
-else:
-	print("Invalid option!!!")
 
 
-#Prints Decoded Message
-dc.print(option)
+
+
+
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description='Decode the Crypto')
+	parser.add_argument(dest='cipher', help='Enter Cipher Text')
+	parser.add_argument('-m', '--mode',default='magic' ,dest='mode', help='Choose the decode mode [default = magic]')
+	parser.add_argument('-v', '--version', action = 'version', version='Decrypto v1.0')
+	
+	args = parser.parse_args()
+
+	cipher = args.cipher
+	mode = args.mode
+
+	#Welcome Message
+	print("\nLet's turn on Decrypto")
+
+
+	#Creating object
+	dc = Decrypto(cipher, mode)
+	dc.attack()
